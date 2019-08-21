24ad6144c3ad5277f705f7274807710fe23bd4d1
hiral.panchal <hiral.panchal@local.dev.wom.com>
2019-08-21 15:49:39 +0530

    REST endpoint exposed which will accept message and will send it to queue. TopicVO represents the message.

--- RabbitMQ-Producer-Service/src/main/java/com/example/demo/RabbitMqProducerServiceApplication.java
+++ RabbitMQ-Producer-Service/src/main/java/com/example/demo/RabbitMqProducerServiceApplication.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
 public class RabbitMqProducerServiceApplication {
@@ -10,4 +20,23 @@ public class RabbitMqProducerServiceApplication {
 		SpringApplication.run(RabbitMqProducerServiceApplication.class, args);
 	}
 
+	@Bean
+	public Queue queue(){
+	
+		Map<String, Object> map = new HashMap<>();
+		map.put("x-max-length", new Integer(50));
+		return new Queue("basic-queue", true, false, false,map);
+	}
+
+	@Bean
+	public DirectExchange exchange(){
+		
+		return new DirectExchange("basic-exchange");
+	}
+	
+	@Bean
+	public Binding binding(Queue queue, DirectExchange exchange){
+		
+		return BindingBuilder.bind(queue).to(exchange).with("route-to-basicqueue");
+    }
 }
