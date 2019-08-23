e7588e3700d5517b780ef5c61cc6d277c3e5d27f
hiral.panchal <hiral.panchal@local.dev.wom.com>
2019-08-23 18:56:27 +0530

    Consumer Service is created.

---
+++ RabbitMQ-Consumer-Service/src/main/java/com/example/demo/RabbitMqConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqConsumerServiceApplication.class, args);
+	}
+
+}
